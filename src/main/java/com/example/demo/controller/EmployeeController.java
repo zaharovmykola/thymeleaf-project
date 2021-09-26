dde5af9abc80a59cbd56364d892a935b06c2d9c8
zaharovmykola <zaharovmykola@gmail.com>
2021-09-26 15:24:40 +0200

    added logic to save Employee

--- src/main/java/com/example/demo/controller/EmployeeController.java
+++ src/main/java/com/example/demo/controller/EmployeeController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -23,10 +25,16 @@ public class EmployeeController {
     }
 
     @GetMapping("/addEmployeeForm")
-    public String addEMployeeForm(Model model){
+    public String addEmployeeForm(Model model){
         Employee newEmployee = new Employee();
         model.addAttribute("employee", newEmployee);
         return "add-employee-form";
+    }
+
+    @PostMapping("/saveEmployee")
+    public String saveEmployee(@ModelAttribute Employee employee){
+        employeeRepository.save(employee);
+        return "redirect:/showEmployees/list";
     }
 
 }
