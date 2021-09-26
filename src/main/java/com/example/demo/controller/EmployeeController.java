2c2b2c3c0c2e033f361d8c173b61358876daeda6
zaharovmykola <zaharovmykola@gmail.com>
2021-09-26 14:15:03 +0200

    EmployeeController showEmployees() created

---
+++ src/main/java/com/example/demo/controller/EmployeeController.java
@@ -0,0 +1,25 @@
+package com.example.demo.controller;
+
+import com.example.demo.entity.Employee;
+import com.example.demo.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @GetMapping("/showEmployees")
+    public String showEmployees(Model model) {
+        List<Employee> listOfEmployyes = employeeRepository.findAll();
+        model.addAttribute("employees", listOfEmployyes);
+        return "list-employees";
+    }
+
+}
